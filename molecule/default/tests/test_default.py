8e23545c3a33d15f9260b2b846e64a3f12aa0d2d
Sebastien Besson <seb.besson@gmail.com>
2018-09-16 19:50:45 +0100

    Migrate test to v2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,19 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_jre_version(host):
+    assert host.exists('java')
+    c = host.run('java -version')
+    assert c.stderr.startswith('openjdk version "1.8.0')
+
+
+def test_jdk_version(host):
+    hostname = host.backend.get_hostname()
+    if hostname == 'java-jdk':
+        assert host.exists('javac')
+        c = host.run('javac -version')
+        assert c.stderr.startswith('javac 1.8.0')
+    else:
+        assert not host.exists('javac')
